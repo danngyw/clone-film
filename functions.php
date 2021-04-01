eb621016e435bc65f25138a997e93f61dcab717f
danngyw <wpcodev@gmail.com>
2021-04-01 10:09:14 +0700

    add index to include files

--- functions.php
+++ functions.php
@@ -5,10 +5,8 @@ include_once('includes/html.php');
 
 include_once('includes/wp_head.php');
 
+require_once TEMPLATEPATH."/index.php";
 
-//require_once TEMPLATEPATH."/includes/craw_import_home_page.php";
-require_once TEMPLATEPATH."/includes/craw_import_pages.php";
-require_once TEMPLATEPATH."/includes/update_fillm_detail.php";
 
 require_once "vendor/autoload.php";
 use FastSimpleHTMLDom\Document;
---
+++ includes/index.php
@@ -0,0 +1,4 @@
+<?php
+require_once dirname(__FILE__) . '/craw_import_pages.php';
+require_once TEMPLATEPATH."/includes/craw_import_home_page.php";
+require_once TEMPLATEPATH."/includes/update_fillm_detail.php";
