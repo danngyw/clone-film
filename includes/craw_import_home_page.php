3eec14470f79a441f2008707f9da31a858d625d2
danngyw <wpcodev@gmail.com>
2021-04-01 10:11:55 +0700

    update path

--- includes/craw_import_home_page.php
+++ includes/craw_import_home_page.php
@@ -110,29 +110,3 @@ function auto_update_film_thumbnail($film_id){
 
     import_film_thumbnail($args, $film_id);
 }
-function manually_update_filmd_thumbnail(){
-    global $wpdb;
-    $args = array(
-        'post_type' => 'film',
-        'meta_query' => array(
-            array(
-             'key' => '_thumbnail_id',
-             'compare' => 'NOT EXISTS'
-            ),
-        ),
-    );
-    $the_query = new WP_Query($args);
-    if ( $the_query->have_posts() ) :
-        while($the_query->have_posts()){
-
-            $the_query->the_post();
-            get_the_title();
-            global $post;
-            $p_id = $post->ID;
-            auto_update_film_thumbnail($p_id);
-
-        }
-    endif;
-
-}
-add_action('wp_footer','manually_update_filmd_thumbnail', 99);
