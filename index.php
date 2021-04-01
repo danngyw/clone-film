357496a8a4170292cd86d374d02f78a5567fb4e3
Dan Ngyen <wpcodev@gmail.com>
2021-04-01 16:29:34 +0700

    update font

--- index.php
+++ index.php
@@ -5,71 +5,21 @@ use FastSimpleHTMLDom\Document;
 get_header();
 ?>
 <div class="container">
-	<?php get_template_part('template/latest','movies');?>
-<div class="row">
-<div class="col-md-8">
+	<?php
+	if( is_home() || is_front_page() ){?>
 
- <?php get_recent_films();?>
 
-</div>
-<div class="col-md-4">
-<h4 class="section-title">Popular</h4>
-<ul class="media-list" itemscope="" itemtype="http://schema.org/Movie">
-	<li class="media media-movie-clickable mmc-tiny">
-	<div class="media-left media-middle"> <a href="/movie-imdb/tt3281548" itemprop="url"> <img class="media-object" src="https://img.yts.mx/assets/images/movies/little_women_2019/small-cover.jpg" alt="Little Women" height="42" itemprop="image"> </a> </div>
-	<div class="media-body">
-	 <a href="/movie-imdb/tt3281548">
-	<h5 class="media-heading" itemprop="name">Little Women (2019)</h5>
-	<small itemprop="genre">Drama, Romance</small>
-	</a>
+		<?php get_template_part('template/latest','movies');?>
+
+	<?php  } ?>
+	<div class="row">
+	<div class="col-md-8">
+
+	 <?php get_recent_films();?>
+
 	</div>
-	</li>
-</ul>
-<h4 class="section-title">Genre</h4>
-<ul class="list-group row default-list">
-<li class="list-group-item col-xs-4"><a href="/genre/action">Action</a></li>
-<li class="list-group-item col-xs-4"><a href="/genre/adventure">Adventure</a></li>
-<li class="list-group-item col-xs-4"><a href="/genre/animation">Animation</a></li>
-<li class="list-group-item col-xs-4"><a href="/genre/biography">Biography</a></li>
-<li class="list-group-item col-xs-4"><a href="/genre/comedy">Comedy</a></li>
-<li class="list-group-item col-xs-4"><a href="/genre/crime">Crime</a></li>
-<li class="list-group-item col-xs-4"><a href="/genre/documentary">Documentary</a></li>
-<li class="list-group-item col-xs-4"><a href="/genre/drama">Drama</a></li>
-<li class="list-group-item col-xs-4"><a href="/genre/family">Family</a></li>
-<li class="list-group-item col-xs-4"><a href="/genre/fantasy">Fantasy</a></li>
-<li class="list-group-item col-xs-4"><a href="/genre/film-noir">Film-Noir</a></li>
-<li class="list-group-item col-xs-4"><a href="/genre/game-show">Game-Show</a></li>
-<li class="list-group-item col-xs-4"><a href="/genre/history">History</a></li>
-<li class="list-group-item col-xs-4"><a href="/genre/horror">Horror</a></li>
-<li class="list-group-item col-xs-4"><a href="/genre/music">Music</a></li>
-<li class="list-group-item col-xs-4"><a href="/genre/musical">Musical</a></li>
-<li class="list-group-item col-xs-4"><a href="/genre/mystery">Mystery</a></li>
-<li class="list-group-item col-xs-4"><a href="/genre/news">News</a></li>
-<li class="list-group-item col-xs-4"><a href="/genre/reality-tv">Reality-TV</a></li>
-<li class="list-group-item col-xs-4"><a href="/genre/romance">Romance</a></li>
-<li class="list-group-item col-xs-4"><a href="/genre/sci-fi">Sci-Fi</a></li>
-<li class="list-group-item col-xs-4"><a href="/genre/sport">Sport</a></li>
-<li class="list-group-item col-xs-4"><a href="/genre/talk-show">Talk-Show</a></li>
-<li class="list-group-item col-xs-4"><a href="/genre/thriller">Thriller</a></li>
-<li class="list-group-item col-xs-4"><a href="/genre/war">War</a></li>
-<li class="list-group-item col-xs-4"><a href="/genre/western">Western</a></li>
-</ul>
-<h4 class="section-title">Language</h4>
-<ul class="list-group row default-list">
-<li class="list-group-item col-xs-6"><a href="/language/albanian">Albanian</a></li>
-<li class="list-group-item col-xs-6"><a href="/language/arabic">Arabic</a></li>
-<li class="list-group-item col-xs-6"><a href="/language/bengali">Bengali</a></li>
-<li class="list-group-item col-xs-6"><a href="/language/brazilian-portuguese">Brazilian Portuguese</a></li>
-<li class="list-group-item col-xs-6"><a href="/language/bulgarian">Bulgarian</a></li>
-<li class="list-group-item col-xs-6"><a href="/language/chinese">Chinese</a></li>
-<li class="list-group-item col-xs-6"><a href="/language/slovenian">Slovenian</a></li>
-<li class="list-group-item col-xs-6"><a href="/language/spanish">Spanish</a></li>
-<li class="list-group-item col-xs-6"><a href="/language/swedish">Swedish</a></li>
-<li class="list-group-item col-xs-6"><a href="/language/thai">Thai</a></li>
-
-</ul>
-</div>
-</div>
+	<?php get_sidebar();?>
+	</div>
 </div>
 <?php
 get_footer();
