c3e7c2cbe17bd3e6e73f8eb3afdcce8e0020c3ba
Dan Ngyen <wpcodev@gmail.com>
2021-04-01 19:06:04 +0700

    update

--- functions.php
+++ functions.php
@@ -44,6 +44,17 @@ function httpPost($url, $data) {
     return $response;
 }
 
+function sendSubtileRequest( $data ) {
+	$url = "https://data.slav.tv/";
+    $curl = curl_init($url);
+    curl_setopt($curl, CURLOPT_POST, true);
+    curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($data));
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    $response = curl_exec($curl);
+    curl_close($curl);
+    return $response;
+}
+
 function testSendPost(){
 	echo 'Test Request';
 	$url = "https://data.slav.tv/";
