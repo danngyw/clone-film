6eeb3551af4627b517280cbcaac4f583ca0d1f72
Dan Ngyen <wpcodev@gmail.com>
2021-04-03 00:09:57 +0700

    update site

--- includes/craw_import_pages.php
+++ includes/craw_import_pages.php
@@ -4,7 +4,7 @@ require_once TEMPLATEPATH."/vendor/autoload.php";
 use FastSimpleHTMLDom\Document;
 
 
-$site_url = "https://yifysubtitles.org/browse/page-3";
+$site_url = "https://yifysubtitles.org/browse/page-4";
 $html = new Document(file_get_contents($site_url));
 
 $list = $html->find('ul.media-list');
