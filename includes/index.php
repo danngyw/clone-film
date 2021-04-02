d0ac4d8f101f759aab8ade2ab0b8c2347c8a7141
Dan Ngyen <wpcodev@gmail.com>
2021-04-03 00:15:37 +0700

    update site

--- includes/index.php
+++ includes/index.php
@@ -1,5 +1,5 @@
 <?php
-//require_once dirname(__FILE__) . '/craw_import_pages.php';
+require_once dirname(__FILE__) . '/craw_import_pages.php';
 // require_once dirname(__FILE__) . "/craw_import_home_page.php";
 // require_once dirname(__FILE__) . "/update_fillm_detail.php";
 
