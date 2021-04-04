7a3df5a7e10df2a2d3ca26ac3f09afc85aba3c2e
Dan Ngyen <wpcodev@gmail.com>
2021-04-04 19:18:04 +0700

    upate

--- admin/film_column.php
+++ admin/film_column.php
@@ -2,12 +2,11 @@
 
 
 function crawl_columns($columns){
-    $columns['crawl_status'] = 'Manual Crawl';
-     return $columns;
+    $columns['manual_crawl'] = 'Manual Crawl';
+    $columns['crawl_status'] = 'Crawl Status';
+    return $columns;
 }
 add_filter('manage_edit-film_columns', 'crawl_columns',99);
-
-
 
 add_action('manage_posts_custom_column',  'show_crawl_status');
 function show_crawl_status($name) {
@@ -16,6 +15,15 @@ function show_crawl_status($name) {
     $link = admin_url('?page=crawl-overview&film_id='.$post->ID);
     switch ($name) {
         case 'crawl_status':
-           echo '<a target="_blank" href="'.$link.'">Update subtitle</a>';
+           	echo '<a target="_blank" href="'.$link.'">Update subtitle</a>';
+           	break;
+          	case 'crawl_status':
+          	$is_full_update = get_post_meta($post->ID,'is_full_updated', true);
+          	if($is_full_update == 'full'){
+          		echo "Yes"
+          	} else {
+          		echo 'No';
+          	}
+           break;
     }
 }
