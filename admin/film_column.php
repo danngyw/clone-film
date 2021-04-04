96f91de8a3c6421761f29471a73ecee06e0262b5
Dan Ngyen <wpcodev@gmail.com>
2021-04-04 19:19:17 +0700

    update

--- admin/film_column.php
+++ admin/film_column.php
@@ -14,13 +14,13 @@ function show_crawl_status($name) {
     global $post;
     $link = admin_url('?page=crawl-overview&film_id='.$post->ID);
     switch ($name) {
-        case 'crawl_status':
+        case 'manual_crawl':
            	echo '<a target="_blank" href="'.$link.'">Update subtitle</a>';
            	break;
           	case 'crawl_status':
           	$is_full_update = get_post_meta($post->ID,'is_full_updated', true);
           	if($is_full_update == 'full'){
-          		echo "Yes"
+          		echo "Yes";
           	} else {
           		echo 'No';
           	}
