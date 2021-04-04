e57149843ea8dce348ce557fb9eb4e3da4cf961a
Dan Ngyen <wpcodev@gmail.com>
2021-04-04 22:37:00 +0700

    update text

--- debug.php
+++ debug.php
@@ -19,5 +19,26 @@ function manual_film_debug(){
 	echo '<pre>';
 	var_dump('Number film need to be crawl:'.$query->post_count);
 	echo '</pre>';
+
+	$args = array(
+		'post_type' => 'subtitle',
+		'post_status' => 'publish',
+
+	    'posts_per_page' => -1,
+
+	);
+	$query = new WP_Query($args);
+	if($query->have_posts() ){
+		while ($query->have_posts() ) {
+			$query->the_post();
+			global $post;
+			$film_id = $post->post_parent;
+			$film  = get_post($film_id);
+			if(!$film){
+				// xóa nhưng substile không có giá trị sử dụng.
+				// wp_delete_post($post->ID, true);
+			}
+		}
+	}
 }
 // add_action('wp_footer','manual_film_debug');
