a1bea7557ea678d8f9e7b2d6fd4c8641d8a608bc
Dan Ngyen <wpcodev@gmail.com>
2021-04-05 20:20:29 +0700

    update sql check

--- header.php
+++ header.php
@@ -1,20 +1,16 @@
 <html>
 	<head>
-		<?php do_action('wp_head');?>
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<?php
 		$title = get_bloginfo('name');
 		$description = get_bloginfo('description');
-		?>
-		<title><?php echo $title;?></title>
-		<meta name="description" content="<?php echo $description;?>">
+		wp_head(); ?>
 		<meta property="og:type" content="website" />
 		<meta property="og:url" content="<?php echo home_url();?>" />
 		<meta property="og:image" content="<?php echo get_stylesheet_directory_uri();?>/images/screenshots/screenshot01.jpg" />
-		<meta property="fb:admins" content="1" />
-		<meta property="fb:app_id" content="1" />
+
 		<link href='//fonts.googleapis.com/css?family=Open+Sans:400,600,800italic,800' rel='stylesheet' type='text/css'>
 		<link href="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/css/toastr.min.css" rel="stylesheet">
 		<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
