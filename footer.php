2c33229443c87334190d8e1b3e382394eef6da6b
Dan Ngyen <wpcodev@gmail.com>
2021-04-05 07:47:33 +0700

    update footer link

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 		<footer class="footer">
 			<div class="container text-center">
 				<div class="row">
-					<div class="col-xs-12"><a href="/privacy">privacy</a> | <a href="/legal-information">legal</a> | <a href="/contact">contact</a></div>
+					<div class="col-xs-12"><a href="<?php echo home_url();?>/privacy">privacy</a> | <a href="<?php echo home_url();?>/legal-information">legal</a> | <a href="<?php echo home_url();?>/contact">contact</a></div>
 					<div class="col-xs-12 text-muted">All images and subtitles are copyrighted to their respectful owners unless stated otherwise. This website is not associated with any external links or websites. ©yifysubtitles. </div>
 				</div>
 			</div>
