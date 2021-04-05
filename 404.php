1fe397a6f65ead587af52958fa8a2a67e991f782
Dan Ngyen <wpcodev@gmail.com>
2021-04-06 00:48:17 +0700

    update

---
+++ 404.php
@@ -0,0 +1,14 @@
+<?php
+get_header();
+?>
+<div class="container">
+	<div class="home-cols">
+
+		<div class="col-md-8">
+			<h1>Error 404 - Not Found</h1>
+			<p><?php _e( 'It looks like nothing was found at this location. Maybe try a search?', 'twentythirteen' ); ?></p>
+		</div>
+		<?php get_sidebar();?>
+	</div>
+</div>
+<?php get_footer(); ?>
