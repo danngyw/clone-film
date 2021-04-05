bbc36df54fc731eb00a3c684c4a0b5e5aa1878cd
Dan Ngyen <wpcodev@gmail.com>
2021-04-05 09:29:57 +0700

    upate

--- admin/init_dashboard.php
+++ admin/init_dashboard.php
@@ -2,7 +2,7 @@
 function crawl_del_subtitle_of_film($film_id){
 	global $wpdb;
 	$sql = "DELETE FROM `{$wpdb->posts}` WHERE  post_type = 'subtitle' AND post_parent = {$film_id}";
-	crawl_log('Delete all subtiles of film.'.$film_id);
+	// crawl_log('Delete all subtiles of film.'.$film_id);
 	$wpdb->query($sql);
 }
 add_action('after_delete_post','crawl_del_subtitle_of_film');
