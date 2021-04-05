3b3450cae679b581fc27b366084d5d6841ad20ea
Dan Ngyen <wpcodev@gmail.com>
2021-04-05 07:36:59 +0700

    update page

--- page.php
+++ page.php
@@ -3,7 +3,7 @@ get_header();
 ?>
 <div class="container">
 	<div class="home-cols">
-		<h2><?php the_title();?>
+		<h2><?php the_title();?></h2>
 		<div class="page-content">
 			<?php the_content();?>
 		</div>
