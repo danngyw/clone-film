75cb442d56b6347f6c1c4e53102f3e9f30e1e31a
Dan Ngyen <wpcodev@gmail.com>
2021-04-23 23:00:09 +0700

    update

--- custom_db.php
+++ custom_db.php
@@ -6,7 +6,7 @@ $charset_collate = $wpdb->get_charset_collate();
 $sql = "CREATE TABLE `{$wpdb->base_prefix}imported_track` (
   	ID bigint(20) NOT NULL AUTO_INCREMENT,
   	film_id bigint(20) UNSIGNED NOT NULL,
-  	source_id bigint(20) NOT NULL,
+  	source_id VARCHAR(20) NOT NULL,
   	PRIMARY KEY  (ID),
   	UNIQUE KEY ID (ID)
 	) $charset_collate;";
